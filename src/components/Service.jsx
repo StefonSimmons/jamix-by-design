0ecba22ed792724fae30ecb1f00dbb3449a76501
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-24 19:36:24 -0400

    styling services grid

--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -1,7 +1,7 @@
 
 export default function Service({ service }) {
   return (
-    <div className="service-card-container">
+    <div className={`service-card-container ${service.name.toLowerCase()}`}>
       <div className="service-card-color">
         <div className="service-card">
           <div className="service-card-content">
