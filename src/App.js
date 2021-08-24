3a3e077e4655e384cd2723ae2b225d44648eee68
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-24 17:13:02 -0400

    about styled

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route } from "react-router-dom";
 import Layout from "./layout/Layout";
+import About from "./screens/About";
 import Home from "./screens/Home";
 import "./styles/main.css"
 
@@ -8,6 +9,9 @@ function App() {
     <Layout>
       <Route exact path="/">
         <Home />
+      </Route>
+      <Route exact path="/about">
+        <About/>
       </Route>
     </Layout>
   );
---
+++ src/screens/About.jsx
@@ -0,0 +1,25 @@
+import joyce from "../assets/about-joyce.jpg"
+import floral from "../assets/about-floral.png"
+
+export default function About() {
+  return (
+    <div className="about-screen">
+      <main className="about-floater">
+        <section className="about-me">
+          <img src={joyce} alt="joyce" />
+          <p>Joyce is the Founder and CEO of JAMix By Design.  Her love and passion for event planning and decorating developed out of her own process in curating family events. She is very detail oriented and will spend every second to make sure your day is stress free.</p>
+        </section>
+        <section className="about-jamix">
+          <h1>About JAMix</h1>
+          <img src={floral} alt="floral arrangement" />
+          <div className="about-jamix-p">
+            <p>JAMix By Design is an event planning, designing and decorating team dedicated to bringing your vision and desires to life. Like the founder, the team works tirelessly to make each day as special as it can be. We take on each event like our very own. Beginning with the first planning meeting, we make your vision our mission.  We strive to capture it all and not miss a beat!</p>
+            <p>The Mix in JAMix represents the array of clients, services, designs, cultures, and atmosphere we aim to bring and serve. Are you thinking of something unique and not cookie cutter? Give us a call today!</p>
+          </div>
+        </section>
+      </main>
+      <div className="about-gold-bg"></div>
+
+    </div>
+  )
+}
