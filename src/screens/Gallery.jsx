f99d7371431c123503d6df6be57180a9630222e7
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-25 12:12:58 -0400

    added pointer to gallery photos

--- src/screens/Gallery.jsx
+++ src/screens/Gallery.jsx
@@ -3,9 +3,16 @@ import { gallery } from '../import-info/gallery'
 export default function Gallery() {
   return (
     <div className="gallery-screen">
-      {/* {gallery.map((photo, idx) => <img key={idx} src={photo} alt="decoration"/>)} */}
       <section className="gallery">
-        {gallery.map((photo, idx) => <div key={idx} className="photo" style={{ backgroundImage: `url(${photo})` }}></div>)}
+        {gallery.map((photo, idx) => (
+          <div
+            key={idx}
+            className="photo"
+            // onClick={}
+            style={{ backgroundImage: `url(${photo})` }}
+          >
+          </div>
+        ))}
       </section>
     </div>
   )
