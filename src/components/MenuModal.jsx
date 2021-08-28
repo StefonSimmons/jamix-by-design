ce9a4ead882b1e7c43dffc882532c3f64a54a337
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2021-08-28 15:58:12 -0400

    mobile menu nav items styled

--- src/components/MenuModal.jsx
+++ src/components/MenuModal.jsx
@@ -1,13 +1,21 @@
 import { Fragment } from "react"
+import { Link } from "react-router-dom"
 import close from "../assets/close-icon.svg"
 
 
 export default function MenuModal({ menuModal, setMenuModal }) {
   return (
     <Fragment>
-      { menuModal &&
-        < div className="menu-modal" >
+      {menuModal &&
+        <div className="menu-modal" >
           <img src={close} alt="close" onClick={() => setMenuModal(false)} />
+          <ul>
+            <Link><li>Home</li></Link>
+            <Link><li>About</li></Link>
+            <Link><li>Gallery</li></Link>
+            <Link><li>Services</li></Link>
+            <Link><li>Contact</li></Link>
+          </ul>
         </div >
       }
     </Fragment>
