5d59e80c7c1e57f6286a0ab030d73b8bc279fc9b
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2023-01-08 01:35:29 -0500

    login component styled

---
+++ client/src/screens/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Login() {
+  return (
+    <div className="login-screen">
+        <form className="login-form">
+            <input type="text" name="" id="" placeholder="Username"/>
+            <input type="password" name="" id="" placeholder="Password"/>
+            <input type="submit" value="Login" />
+        </form>
+    </div>
+  )
+}
