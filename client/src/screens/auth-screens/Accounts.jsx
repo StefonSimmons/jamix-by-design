53e6be1a4c87e685559b205f28cab93f577ea330
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2023-01-10 00:00:43 -0500

    move auth screens

--- client/src/screens/auth-screens/Accounts.jsx
+++ client/src/screens/auth-screens/Accounts.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {useEffect, useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-import {url, config} from '../services/apiConfig'
+import {url, config} from '../../services/apiConfig'
 
 export default function Accounts({destroyUsers, updateUsers, user}) {
     const navigate = useNavigate()
