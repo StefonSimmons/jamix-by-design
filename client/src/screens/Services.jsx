d3a5bca5f091e6bdd1019025ed6d755fe8ca7dce
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2023-01-11 17:56:40 -0500

    rm errs

--- client/src/screens/Services.jsx
+++ client/src/screens/Services.jsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from "react"
+import { Fragment } from "react"
 import Service from "../components/Service"
 import { services } from "../import-info/services"
 
