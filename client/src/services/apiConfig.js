c5b31568198c3da634f39ce9aee60e974fca2af7
stefonsimmons <s.j.simmons@spartans.nsu.edu>
2023-01-11 18:49:48 -0500

    use `render` server

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -4,7 +4,7 @@ export const atAPI = axios.create({
   baseURL: `https://api.airtable.com/v0/${process.env.REACT_APP_AIRTABLE_BASE}`
 })
 export const api = axios.create({
-  baseURL: "http://localhost:3001"
+  baseURL: process.env.NODE_ENV === "production" ? "https://jamix-server.onrender.com":"http://localhost:3001"
 })
 export const config = {
   headers: {
